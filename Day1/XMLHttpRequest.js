cbbf66f489a559bca6c8c7a80488e8ae3e6975d1
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-07-06 13:01:42 +0530

    Day7 HTML starter

--- Day1/XMLHttpRequest.js
+++ Day1/XMLHttpRequest.js
@@ -20,21 +20,21 @@
 
 // console.log(todoRequest);
 
-var todosAPi = "https://jsonplaceholder.typicode.com/users/";
-
+var todosAPi = "https://restcountries.com/v3.1/all";
 var todoRequest = new XMLHttpRequest();
 
 todoRequest.onreadystatechange = function () {
   var data = JSON.parse(this.response);
   console.log(data);
-  //   if (todos.length > 0) {
-  //     for (var x = 0; x < todos.length; x++) {
-  //       console.log(todos[x].title);
-  //     }
-  //   }
+  filterAsianCountries(data);
+  filterPopulation(data, 200000);
 };
 
 todoRequest.open("GET", todosAPi); // GET, POST, PUT, DELETE, PATCH
 todoRequest.send();
 
 console.log(todoRequest);
+
+function filterAsianCountries(data = []) {}
+
+function filterPopulation(data = [], population = 0) {}
