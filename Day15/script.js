43617c7def7e946f5975a2399486cc644e2a490d
vaaluvishnu5146 <vaaluvishnu5146@gmail.com>
2024-08-04 13:06:49 +0530

    Day15 Async day 1

---
+++ Day15/script.js
@@ -0,0 +1,79 @@
+// function compute(a, b, cb) {
+//     console.log('Compute Started')
+//     setTimeout(() => {
+//         cb(a + b, 5)
+//     }, 5000)
+//     console.log('Compute Ended')
+// }
+
+// function makeDecision(ans = 0, checkValue = 0) {
+//     if (ans > checkValue) {
+//         console.log(`Greater than ${checkValue}`)
+//     } else {
+//         console.log(`Lesser than ${checkValue}`)
+//     }
+// }
+
+// compute(2, 3, (a, b) => {
+//     console.log(a, b)
+// })
+
+// var b = 10;
+
+// function a(value, cb) {
+//     cb('Successfull')
+// }
+
+// function b(message) {
+//     console.log(message)
+// }
+
+// a(10, b);
+
+
+// const a = (b) => {
+//     b(c)
+// }
+
+// const b = (c) => {
+//     c(d)
+// }
+
+// const c = (d) => {
+//     d("Hello")
+// }
+
+// const d = (message) => {
+//     console.log(message)
+// }
+
+// a(b)
+
+
+// 1. Note readable - Not possible
+// 2. Maintainable code - Not possible
+// 3. Code debuggable - Not possible
+const Timer = () => {
+    console.log(10)
+    setTimeout(() => {
+        console.log(9);
+        setTimeout(() => {
+            console.log(8);
+            setTimeout(() => {
+                console.log(7);
+            }, 1000)
+        }, 1000)
+    }, 1000)
+}
+
+Timer()
+
+
+
+
+
+
+
+
+
+
